176cc795b82294f78a9c69ca4b52bff8d4bf08de
felix-t-morgenstern <44931803+felix-t-morgenstern@users.noreply.github.com>
2020-03-06 02:51:58 -0500

    POC with viable texturing

---
+++ src/Shader.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import static org.lwjgl.opengl.GL20.*;
+
+public class Shader {
+    private int _program;
+    private int _vertexShader;
+    private int _fragmentShader;
+
+    public Shader(String filename) {
+        _program = glCreateProgram();
+
+        _vertexShader = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(_vertexShader, readFile(filename + ".vertex"));
+        if (glGetShaderi(_vertexShader, GL_COMPILE_STATUS) != 1) {
+            throw new IllegalArgumentException(glGetShaderInfoLog(_vertexShader));
+        }
+
+        _fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(_fragmentShader, readFile(filename + ".fragment"));
+        glCompileShader(_fragmentShader);
+        if (glGetShaderi(_fragmentShader, GL_COMPILE_STATUS) != 1) {
+            throw new IllegalArgumentException(glGetShaderInfoLog(_fragmentShader));
+        }
+
+        glAttachShader(_program, _vertexShader);
+        glAttachShader(_program, _fragmentShader);
+
+        glBindAttribLocation(_program, 0, "vertices");
+
+        glLinkProgram(_program);
+
+        if (glGetProgrami(_program, GL_LINK_STATUS) != 1) {
+            throw new IllegalArgumentException(glGetShaderInfoLog(_program));
+        }
+
+        glValidateProgram(_program);
+
+        if (glGetProgrami(_program, GL_VALIDATE_STATUS) != 1) {
+            throw new IllegalArgumentException(glGetShaderInfoLog(_program));
+        }
+    }
+
+    public void bind() {
+        glUseProgram(_program);
+    }
+
+    private String readFile(String filename) {
+        StringBuilder builder = new StringBuilder();
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(new File("./shaders/" + filename)));
+            String line;
+            while((line = reader.readLine()) != null) {
+                builder.append(line);
+                builder.append("\n");
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return builder.toString();
+    }
+}
---
+++ src/Texture.java
@@ -0,0 +1,61 @@
+import org.lwjgl.BufferUtils;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+import static org.lwjgl.opengl.GL12.*;
+import static org.lwjgl.opengl.GL12.GL_CLAMP_TO_EDGE;
+
+public class Texture {
+    private int id;
+    private int width;
+    private int height;
+
+    public Texture(String filename) {
+        BufferedImage bi;
+        try {
+            bi = ImageIO.read(new File(filename));
+
+            width = bi.getWidth();
+            height = bi.getHeight();
+
+            int[] pixels_raw = new int[width * height * 4];
+            pixels_raw = bi.getRGB(0, 0, width, height, null, 0, width);
+
+            ByteBuffer pixels = BufferUtils.createByteBuffer(width * height * 4);
+
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    int pixel = pixels_raw[i* width + j];
+                    pixels.put((byte)((pixel >> 16) & 0xFF)); // r
+                    pixels.put((byte)((pixel >> 8) & 0xFF)); // g
+                    pixels.put((byte)(pixel & 0xFF)); // b
+                    pixels.put((byte)((pixel >> 24) & 0xFF)); // a
+                }
+            }
+
+            pixels.flip();
+
+            id = glGenTextures();
+
+            glBindTexture(GL_TEXTURE_2D, id);
+
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e.getMessage());
+        }
+    }
+
+    public void bind() {
+        glBindTexture(GL_TEXTURE_2D, id);
+    }
+}
